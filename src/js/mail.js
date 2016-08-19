85a5218d25cc0582dc2c388d6ba5d746f61e96aa
Dylan Harness <dharness@uwo.ca>
2016-08-19 03:12:04 -0400

    updated error responses

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,28 +1,24 @@
 function sendMail(formSelector) {
   var form = $(formSelector);
-  console.log(form[0].checkValidity());
   if (form[0].checkValidity()) {
     var formData = form.serialize();
     alert(formData);
     $('.form-el').val('');
     form.removeClass('erorr');
+    $.ajax({
+      method: 'POST',
+      dataType: 'json',
+      data: formData,
+      url: 'https://formspree.io/dharness.student@gmail.com',
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(data) {
+        alert(JSON.stringify(data));
+      }
+    });
   } else {
     form.addClass('erorr');
-    $('.email-input').focus();
+    $(formSelector + '> .email-input').focus();
   }
-  // $.ajax({
-  //   method: 'POST',
-  //   dataType: 'json',
-  //   data: formData,
-  //   url: 'https://formspree.io/dharness.student@gmail.com',
-  //   success: function(data) {
-  //     console.log(data);
-  //   },
-  //   success: function(data) {
-  //     alert(JSON.stringify(data));
-  //   },
-  //   error: function(data) {
-  //     alert(JSON.stringify(data));
-  //   }
-  // });
 }
