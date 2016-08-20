df24ff897a62eebcec1b1f58dc3d4b49e8fb1762
Dylan Harness <dharness@uwo.ca>
2016-08-19 21:08:08 -0400

    added delayed load of body

--- src/js/mail.js
+++ src/js/mail.js
@@ -5,19 +5,19 @@ function sendMail(formSelector) {
     $(formSelector + '> .form-el').val('');
     form.removeClass('erorr');
     showToast();
-    // $.ajax({
-    //   method: 'POST',
-    //   dataType: 'json',
-    //   data: formData,
-    //   url: 'https://formspree.io/dharness.student@gmail.com',
-    //   success: function(data) {
-    //     console.log('Email sent: ' + data);
-    //   },
-    //   error: function(data) {
-    //     console.log('data ----- ');
-    //     alert(JSON.stringify(data));
-    //   }
-    // });
+    $.ajax({
+      method: 'POST',
+      dataType: 'json',
+      data: formData,
+      url: 'https://formspree.io/dharness.student@gmail.com',
+      success: function(data) {
+        console.log('Email sent: ' + data);
+      },
+      error: function(data) {
+        console.log('data ----- ');
+        alert(JSON.stringify(data));
+      }
+    });
   } else {
     form.addClass('erorr');
     $(formSelector + '> .email-input').focus();
