9023bcc5169e90b7f8849a6abc08fbf815001138
Dylan Harness <dharness@uwo.ca>
2016-08-20 16:30:54 -0400

    promoted

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,3 +8,18 @@ window.onscroll = function (e) {
     nav.classList.remove("scroll-view");
   }
 }
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 40
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
