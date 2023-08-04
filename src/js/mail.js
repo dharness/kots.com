ccbe4af9d70dd76cdb8b54a3317d64a670addacd
Dylan Harness <dylan@NailasMacStudio.attlocal.net>
2023-08-04 12:00:48 -0700

    .

--- src/js/mail.js
+++ src/js/mail.js
@@ -2,24 +2,22 @@ function sendMail(formSelector) {
   var form = $(formSelector);
   if (form[0].checkValidity()) {
     var formData = form.serialize();
-    $(formSelector + '> .form-el').val('');
-    form.removeClass('erorr');
+    $(formSelector + "> .form-el").val("");
+    form.removeClass("erorr");
     $.ajax({
-      method: 'POST',
-      dataType: 'json',
+      method: "POST",
+      dataType: "json",
       data: formData,
-      url: 'https://formspree.io/dharness.student@gmail.com',
-      success: function(data) {
-        // showToast();
-        console.log('Email sent: ' + data);
+      url: "https://formspree.io/f/dharness.student@gmail.com",
+      success: function (data) {
+        console.log("Email sent: " + data);
       },
-      error: function(data) {
-        console.log('data ----- ');
-        alert(JSON.stringify(data));
-      }
+      error: function (data) {
+        console.log(JSON.stringify(data));
+      },
     });
   } else {
-    form.addClass('erorr');
-    $(formSelector + '> .email-input').focus();
+    form.addClass("erorr");
+    $(formSelector + "> .email-input").focus();
   }
 }
